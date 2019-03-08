70c759b69b4c71c4c5517a1d002efbc1ec36df99
Audrey Hayes <audreyhayes0@gmail.com>
2019-03-08 11:43:12 +0000

    added account page

--- login.php
+++ login.php
@@ -1,2 +1,17 @@
 <?php
 
+session_start();
+
+$username = $_POST['uname'];
+$password = $_POST['psw'];
+
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$_SESSION['username'] = $username;
+$_SESSION['password'] = $hash;
+
+$correctLogin = password_verify($password, $hash);
+
+session_destroy();
+
+
